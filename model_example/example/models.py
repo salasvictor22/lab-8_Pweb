72d1c30220d3a6cc54e12eaceb49d6d51eb3d391
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-13 23:17:59 -0500

    En el modelo 'Language y Framework se creo un metodo '__str__' que devuelve el nombre del lenguaje y del framework

--- model_example/example/models.py
+++ model_example/example/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Language(models.Model):
     name = models.CharField(max_length=10)
     
+    def __str__(self):
+        return self.name
+    
 class Framework(models.Model):
     name = models.CharField(max_length=10)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name

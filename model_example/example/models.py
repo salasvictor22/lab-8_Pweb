00e515c16ee1399c6794f14c8e8690ccef994c26
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-13 22:36:50 -0500

    Definiendo el modelo (Framework) y una clave foranea language que establece una relacion de uno a muchos con el modelo (Language)

--- model_example/example/models.py
+++ model_example/example/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)

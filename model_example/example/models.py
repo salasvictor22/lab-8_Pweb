2392106c45fddb27b4ceb62612bcaa36f6901e3c
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-13 23:06:59 -0500

    Creando instancias de Language y Framework, asignando el lenguaje 'Python' a los frameworks 'Django', 'Flask' y 'Bottle', y guardándolos en la base de datos, ademas se creo y se guardo en la base de datos una instancia del modelo Language con el nombre 'java'

--- model_example/example/models.py
+++ model_example/example/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Language(models.Model):
     name = models.CharField(max_length=10)
+    
 class Framework(models.Model):
     name = models.CharField(max_length=10)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)

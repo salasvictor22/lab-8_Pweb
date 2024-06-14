e7549f4542031e3ca96ce0b547365e82fdb4e93d
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-13 23:47:04 -0500

    Definiendo el modelo 'Movie' y el modelo 'Character' con un campo name de tipo cadena y una relacion de muchos a muchos con el modelo 'Movie'

--- model_example/example/models.py
+++ model_example/example/models.py
@@ -13,3 +13,16 @@ class Framework(models.Model):
     
     def __str__(self):
         return self.name
+        
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name

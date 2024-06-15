f71b60b5ba02e238a8e054d707149082e2013eff
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-15 16:45:42 -0500

    Configurando Django para enviar correos electrónicos utilizando el servidor SMTP de hotmail con autenticación TLS en el puerto 587 y modificando el archivo views.py para enviar un correo electronico

--- emailexample/send/views.py
+++ emailexample/send/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+
+    send_mail(
+        'Hello from Arequipa', 
+        'Hello there. This is an automated message.',
+        'salasvictor123@hotmail.com',
+        ['secejap327@fna6.com'],
+        fail_silently=False
+    )
     return render(request, 'send/index.html')

2fd69917ed3a356a8b159bf53f8a78e8ac1ff1ad
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-15 16:17:05 -0500

    Verificando si existe un parámetro download en la solicitud GET y, si está presente, configurando el encabezado Content-Disposition para forzar la descarga del archivo con un nombre específico

--- render_to_pdf/render_to_pdf/views.py
+++ render_to_pdf/render_to_pdf/views.py
@@ -19,6 +19,9 @@ class GeneratePDF(View):
             response = HttpResponse(pdf, content_type='application/pdf')
             filename = "Invoice_%s.pdf" %("12341231")
             content = "inline; filename=%s" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename=%s" %(filename)
             response['Content-Disposition'] = content
             return response
         return HttpResponse("Not found")
